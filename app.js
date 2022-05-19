d21b24a089ceeb5e5b98da1f800c6447fa47465d
Carl <wilkinson.carl123@gmail.com>
2022-05-18 21:09:16 -0400

    es6HomeWork

---
+++ app.js
@@ -0,0 +1,131 @@
+
+
+
+
+// problem #1
+
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+// const num1 =1;
+// const num2 =0;
+
+// function addTwo(num1, num2) {
+//     return num1 + num2;
+// };
+
+// console.log(addTwo(a, b));
+
+
+
+
+// problem #2
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+// const num1 =1;
+// const num2 =0;
+
+// function minusTwo(num1, num2) {
+//     return num1 - num2;
+// };
+
+// console.log(minusTwo(a, b));
+
+// problem # 3
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+// const num1 =1;
+// const num2 =0;
+
+// function timesTwo(num1, num2) {
+//     return num1 * num2;
+// };
+
+// console.log(timesTwo(a, b));
+
+
+// problem #4
+
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+// const num1 =1;
+// const num2 =0;
+
+// function divideTwo(num1, num2) {
+//     return num1 / num2;
+// };
+
+// console.log(divideTwo(a, b));
+
+
+// arrow problems # 1-4
+
+// problem #1
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+
+
+// newAdd = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// console.log(newAdd(a,b));
+
+// problem #2
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+
+
+// newMinus = (num1, num2) => {
+//     return num1 - num2;
+// }
+
+// console.log(newMinus(a,b));
+
+// problem #3
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+
+newDivide = (num1, num2) => {
+    return num1 / num2;
+}
+
+console.log(newDivide(a,b));
+
+// problem #4
+
+// const a = 5;
+// const b = 4;
+// const c = 3;
+// const d = 2;
+
+
+// newTimes = (num1, num2) => {
+//     return num1 * num2;
+// }
+
+// console.log(newTimes(a,b));
+
+
+
+
